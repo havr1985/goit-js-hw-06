cafc7054f71fe88f384668f956013371599a3035
havr1985 <gavrtel1985@gmail.com>
2023-07-23 17:05:34 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const items = [];
+for (let ingredient of ingredients) {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  items.push(item);
+}
+items.forEach(item => list.append(item));
