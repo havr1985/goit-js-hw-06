020fbe0c853e56b2b3cefc2f0b50d1570cd6ba26
havr1985 <gavrtel1985@gmail.com>
2023-07-29 21:47:42 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+const bodyElement = document.querySelector('body');
+
+button.addEventListener('click', onClickBtn);
+
+function onClickBtn() {
+  let hexColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = hexColor;
+  textColor.textContent = hexColor;
+}
