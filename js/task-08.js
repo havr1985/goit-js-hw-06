53e80fab035b16908768446ebaf93cf88bec9542
havr1985 <gavrtel1985@gmail.com>
2023-07-29 21:25:55 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    
+    if (!(email.value) || !(password.value)) {
+        alert("Не всі поля заповнені!");
+        return;
+    }
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(data);
+    form.reset();
+}
